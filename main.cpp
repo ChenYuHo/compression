71ca3465c5147d6c84a670581acec3afe2584975
Chen-Yu Ho <chenyu.ho@kaust.edu.sa>
2020-12-01 13:29:12 +0300

    Remove random number generation and cxxopt

--- main.cpp
+++ main.cpp
@@ -3,49 +3,10 @@
 #include <fstream>
 #include <iterator>
 #include <cmath>
-#include <random>
 #include <chrono>
-#include "include/cxxopts.h"
 
 using namespace std;
 using namespace std::chrono;
-using namespace cxxopts;
-
-ParseResult parse(int argc, char *argv[]) {
-    Options options(argv[0], "compression");
-    options.add_options()
-            ("i,input", "input file name, randomly generate elements if not provided", value<string>())
-            ("o,data-output", "output file name to save original_data (no-op if given input file)",
-             value<string>())
-            ("r,compressed-output", "output file name to save compressed compressed_data", value<string>())
-            ("decompressed-output", "output file name to save decompressed compressed_data", value<string>())
-            ("m,method", "compress method", value<string>()->default_value("intml"))
-#ifdef DEBUG
-    ("s,size", "number of float32 elements",
-value<uint32_t>()->default_value("100"))
-    ("repeat", "repeat compression and/or decompression", value<uint32_t>()->default_value("1"))
-#else
-            ("s,size", "number of float32 elements",
-             value<uint32_t>()->default_value("26214400")) // default: 100 MB
-            ("repeat", "repeat compression and/or decompression", value<uint32_t>()->default_value("10"))
-#endif
-            ("p,print", "print original, compressed, and decompressed data")
-            ("c,compress", "do and measure compression")
-            ("d,decompress", "do and measure decompression")
-            ("h,help", "print usage");
-    try {
-        auto result = options.parse(argc, argv);
-        if (result.count("help")) {
-            cout << options.help() << endl;
-            exit(0);
-        }
-        return result;
-    } catch (const OptionException &e) {
-        cout << "error parsing options: " << e.what() << endl;
-        cout << options.help() << endl;
-        exit(1);
-    }
-}
 
 template<typename T>
 class Compressor {
@@ -106,15 +67,8 @@ public:
     };
 
     void generate_data() override {
-        random_device rd;
-        mt19937 e2(rd());
-        normal_distribution<float> dist(0., 0.001);
-        auto gen = [&dist, &e2]() {
-            return dist(e2);
-        };
         original_data.assign(num_elements, 0);
         init_compressed_data();
-        generate(original_data.begin(), original_data.end(), gen);
     }
 
     inline void init_compressed_data() override {
@@ -169,15 +123,8 @@ private:
     }
 
     static inline void compress_one(const float *value, uint8_t *compressed) {
-        thread_local static random_device rd;
-        thread_local static mt19937 e2(rd());
-        thread_local static uniform_real_distribution<float> dist(1, 2);
         int8_t exp = ilogbf(*value);
-#ifndef NORANDOM
-        if (dist(e2) < get_prob(*value)) exp += 1; // add 1
-#else
         if (1.5 < get_prob(*value)) exp+=1; // add 1
-#endif
         if (exp >= 17) *compressed = 127;
         else if (exp <= -110 || *value == 0) *compressed = 0;
         else *compressed = uint8_t(exp + 110);
@@ -208,15 +155,10 @@ private:
     }
 };
 
-void do_work(const ParseResult &result, Compressor<float> &compressor) {
-    if (result.count("input")) {
-        compressor.read_data(result["input"].as<string>());
-    } else {
-        compressor.generate_data();
-    }
-    uint32_t count = result["repeat"].as<uint32_t>();
-    uint32_t num_elements = result["size"].as<uint32_t>();
-    if (result.count("compress")) {
+void do_work(Compressor<float> &compressor) {
+    compressor.generate_data();
+    uint32_t count = 10;
+    uint32_t num_elements = 26214400;
         for (unsigned i = 1; i <= count; ++i) {
             compressor.init_compressed_data();
             high_resolution_clock::time_point begin = high_resolution_clock::now();
@@ -226,8 +168,6 @@ void do_work(const ParseResult &result, Compressor<float> &compressor) {
             cout << "compress round " << i << ": " << ns_taken << " nanoseconds elapsed, "
                  << double(num_elements) / ns_taken * 1e9 << " element/s\n";
         }
-    }
-    if (result.count("decompress")) {
         for (unsigned i = 1; i <= count; ++i) {
             compressor.init_decompressed_data();
             high_resolution_clock::time_point begin = high_resolution_clock::now();
@@ -237,30 +177,10 @@ void do_work(const ParseResult &result, Compressor<float> &compressor) {
             cout << "decompress round " << i << ": " << ns_taken << " nanoseconds elapsed, "
                  << double(num_elements) / ns_taken * 1e9 << " element/s\n";
         }
-    }
-    if (result["print"].as<bool>() && !result.count("no-print")) {
-        compressor.print();
-    }
-    if (!result.count("input") && result.count("data-output")) {
-        compressor.write_original_data(result["data-output"].as<string>());
-    }
-    if (result.count("compressed-output")) {
-        compressor.write_compressed_data(result["compressed-output"].as<string>());
-    }
-    if (result.count("decompressed-output")) {
-        compressor.write_decompressed_data(result["decompressed-output"].as<string>());
-    }
 }
 
 int main(int argc, char *argv[]) {
-    auto result = parse(argc, argv);
-    auto method = result["method"].as<string>();
-    if (method == "intml") {
-        IntMLCompressor compressor(result["size"].as<uint32_t>());
-        do_work(result, compressor);
-    } else {
-        cerr << method << " not implemented" << endl;
-        return 1;
-    }
+    IntMLCompressor compressor(26214400);
+    do_work(compressor);
     return 0;
 }
